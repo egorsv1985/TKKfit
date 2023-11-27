a50c45f36925d0e624bd460a6d44b4c7fa292fd7
egorsv1985 <egorvs2009@gmail.com>
2023-11-27 14:53:45 +0300

    added section promo without adaptive

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,8 +6,17 @@ $(document).ready(function () {
     speed: 500,
     fade: true,
     cssEase: "linear",
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 5000,
     swipe: true,
+    arrows: false,
+  });
+  // Добавляем обработчики событий для кастомных кнопок
+  $(".slider__prev").on("click", function () {
+    $(".slider").slick("slickPrev");
+  });
+
+  $(".slider__next").on("click", function () {
+    $(".slider").slick("slickNext");
   });
 });
