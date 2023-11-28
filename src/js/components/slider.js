d6c023486012381e59c933f422058a05fd6c29ae
egorsv1985 <egorvs2009@gmail.com>
2023-11-28 14:23:03 +0300

    added start section results

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -12,10 +12,30 @@ $(document).ready(function () {
   });
   // Добавляем обработчики событий для кастомных кнопок
   $(".slider__prev").on("click", function () {
-    $(".slider").slick("slickPrev");
+    $(".promo__slider").slick("slickPrev");
   });
 
   $(".slider__next").on("click", function () {
-    $(".slider").slick("slickNext");
+    $(".promo__slider").slick("slickNext");
+  });
+  $(".results__slider").slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    autoplay: false,
+    autoplaySpeed: 5000,
+    swipe: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+  // Добавляем обработчики событий для кастомных кнопок
+  $(".slider__prev").on("click", function () {
+    $(".results__slider").slick("slickPrev");
+  });
+
+  $(".slider__next").on("click", function () {
+    $(".results__slider").slick("slickNext");
   });
 });
