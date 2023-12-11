8b6bf52c8ef45ebf65c0a617612f8dbb26af0db3
egorsv1985 <egorvs2009@gmail.com>
2023-12-11 15:56:57 +0300

    fix

--- public_html/views/parts/footer.blade.php
+++ public_html/views/parts/footer.blade.php
@@ -73,17 +73,19 @@
 
 				}',
 				'formTpl'=>'@CODE:
+
 				<form class="form" method="post">
 					<input type="hidden" name="formid" value="callback">
 					<div class="form-group position-relative mb-3[+name.errorClass+][+name.requiredClass+]">
 						<label for="name" class="form-label form__label fs-16 position-absolute top-50 control-label d-none">* Имя</label>
-						<input type="text" class="form-control form__input rounded-2 p-3 h-100" id="name" placeholder="Имя*" name="name" value="[+name.value+]">
+						<input type="text" class="form-control form__input rounded-2 p-3 h-100" id="name" placeholder="Имя*" name="name" value="[+name.value+]" required="required">
 						<span class="text-white">[+name.error+]</span>
 					</div>
 					<div class="form-group position-relative mb-3[+phone.errorClass+][+phone.requiredClass+]">
 						<label for="phone" class="form-label form__label fs-16 position-absolute top-50 control-label d-none">* Телефон</label>
-						<input type="tel" class="form-control form__input rounded-2 p-3 h-100" id="phone" placeholder="Телефон*" name="phone" value="[+phone.value+]"> [+phone.error+]
+						<input type="tel" class="form-control form__input rounded-2 p-3 h-100" id="phone" placeholder="Телефон*" name="phone" value="[+phone.value+]" required="required"> [+phone.error+]
 					</div>
+					[+form.messages+]
 					<button type="submit" class="form__btn w-100"><i class="glyphicon glyphicon-envelope"></i> Отправить </button>
 				</form>',
 				'to'=>'egorvs2009@gmail.com',
@@ -94,8 +96,8 @@
 				<p>Имя: [+name.value+]</p>
 				<p>Телефон: [+phone.value+]</p>
 				',
-				'errorClass'=>' has-error',
-				'requiredClass'=>' has-warning',
+				'errorClass'=>'has-error',
+				'requiredClass'=>'has-warning',
 				'subject'=>'Новое сообщение',
 				'messagesOuterTpl'=>'@CODE:<div class="alert alert-danger" role="alert">[+messages+]</div>',
 				'errorTpl'=>'@CODE:<span class="help-block">[+message+]</span>',
